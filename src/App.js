1eddd77c8ba7bfe67efa8a4ad9b6025a6d1f2eb8
Yabel <yabelrr@gmail.com>
2023-09-20 13:18:25 +0200

    Header implemented

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import HomePage from "./pages/HomePage";
 import BeersList from "./pages/BeersList";
 import BeerDetails from "./pages/BeerDetails";
 import NewBeer from "./pages/NewBeer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/beers" element={<BeersList/>}/>
-        <Route path="/random-beer" element={<BeerDetails/>}/>
-        <Route path="/new-beer" element={<NewBeer/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/beers" element={<BeersList />} />
+        <Route path="/random-beer" element={<BeerDetails />} />
+        <Route path="/new-beer" element={<NewBeer />} />
       </Routes>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Link to="/" style={{ textDecoration: "none" }}>
+      <nav className="navbar bg-body-tertiary ">
+        <div className="container-fluid bg-primary">
+          <span className="navbar-brand mb-0 h1 text-white">Iron Beers</span>
+        </div>
+      </nav>
+    </Link>
+  );
+};
 
-export default Header
+export default Header;
